efe5502486a65337ffc5aa15c46605457a060d24
sayed <sayed@cems-it.com>
2018-10-10 16:38:54 +0200

    add softDelete to model

--- app/Category.php
+++ app/Category.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
 
 	use SoftDeletes;
     protected $dates = ['deleted_at'];
-    
+
     public function user()
     {
         return $this->belongsToMany('App\User','user_category','categories_id','user_id');
