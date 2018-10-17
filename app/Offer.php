aa2163ff4d1a4195b62b3912bf55a74369d9fb57
sayed <sayed@cems-it.com>
2018-10-17 15:12:01 +0200

    add default

--- app/Offer.php
+++ app/Offer.php
@@ -20,7 +20,7 @@ class Offer extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\User','user_id','id');
+        return $this->belongsTo('App\User','user_id','id')->withDefault(['name' => 'removed']);
     }
 
      public function influncer()
