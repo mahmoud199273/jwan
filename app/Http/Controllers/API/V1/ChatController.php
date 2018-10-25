6452366f8e45df6d4fa1d4e96d9d25928ce33530
Ahmed Atef <>
2018-10-25 22:10:11 +0300

    test

--- app/Http/Controllers/API/V1/ChatController.php
+++ app/Http/Controllers/API/V1/ChatController.php
@@ -91,6 +91,7 @@ public function store(Request $request)
             // error you can't start this
             return $this->setStatusCode(422)->respondWithError('you cant start offer');
           }
+          dd($offer);
           $from_user_id = $user->id;
           $to_user_id = $offer->influcner_id;
         }
