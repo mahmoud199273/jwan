d854f9aa2ac90c847ff08e9c1a3af4d8e62dd2e6
sayed <sayed@cems-it.com>
2018-10-30 11:56:53 +0200

    update chact

--- app/Chat.php
+++ app/Chat.php
@@ -31,5 +31,10 @@ class Chat extends Model
     protected $hidden = [
         'created_at', 'updated_at'
     ];
+     protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 
 }
