97d7d6dbc2e77329752c8d557819dbb43700bb9b
sayed <sayed@cems-it.com>
2018-10-30 15:57:28 +0200

    remove influncer route

--- routes/api.php
+++ routes/api.php
@@ -82,7 +82,6 @@ Route::post('user/send/complanit', 'API\V1\ComplaintsController@store');
 
 Route::post('user/update/player_id'	, 'API\V1\UserController@updatePlayerId');
 
-Route::post('influncer/update/player_id'	, 'API\V1\UserController@updatePlayerId');
 
 Route::get('user/notifications','API\V1\UserController@getNotifications');
 
