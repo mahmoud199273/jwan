739419863b057bbceb34305cda868bd730db1eef
Ahmed Atef <>
2018-11-07 16:03:09 +0300

    no message

--- app/Http/Controllers/API/V1/TransactionsController.php
+++ app/Http/Controllers/API/V1/TransactionsController.php
@@ -54,7 +54,7 @@ class TransactionsController extends Controller
         if ( $request->limit ) {
           $this->setPagination($request->limit);
         }
-        $pagination =  Transactions::SELECT('transactions.*', 'campaign.title', 'offers.*')
+        $pagination =  Transactions::SELECT('transactions.*', 'campaigns.title', 'offers.*')
                                     ->where('transactions.user_id', $user->id)
                                     ->join('users', 'users.id', '=', 'transactions.user_id')
                                     ->leftJoin('campaigns', 'campaigns.id', '=', 'transactions.id')
