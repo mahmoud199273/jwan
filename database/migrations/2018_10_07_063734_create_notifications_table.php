140ee8a2243b88d037071ca16fc0226ebda94a4e
mahmoud73 <m.madbolie@cems-t.com>
2018-12-24 16:11:49 +0200

    transaction commission

--- database/migrations/2018_10_07_063734_create_notifications_table.php
+++ database/migrations/2018_10_07_063734_create_notifications_table.php
@@ -15,7 +15,7 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->increments('id');
-             $table->integer('user_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('message');
             $table->enum('is_seen', [0,1]);
