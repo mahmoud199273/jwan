2e8189ad0e53bb065a19c4fc3294fb91f0e7d874
mahmoud73 <m.madbolie@cems-t.com>
2019-01-14 17:43:39 +0200

    login attempts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         
         Schema::defaultStringLength(191);
