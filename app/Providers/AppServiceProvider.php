5d513309cb9812f20f5fb2502f9bece222dae7b2
mahmoud73 <m.madbolie@cems-t.com>
2019-01-14 17:42:58 +0200

    login attempts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,11 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        //
-        //if(env('REDIRECT_HTTPS'))
-        //{
-            $url->forceSchema('https');
-        //}
+        
         Schema::defaultStringLength(191);
         //date_default_timezone_set('Asia/Riyadh');
         $inactive_users=DB::table('users')->where([['is_active','0'],['account_type','0'],['deleted_at',NULL]])->count();
