9505df52e3e2f5cdf4b04fd1cb9dad50bdebeab1
sayed <sayed@cems-it.com>
2019-02-07 15:13:44 +0200

    crop image

--- resources/views/admin/offers/show.blade.php
+++ resources/views/admin/offers/show.blade.php
@@ -180,7 +180,7 @@
                                         $float="float:right";
                                         if(isset($chat->from_user->account_type) &&$chat->from_user->account_type == "1") $float="float:left";
                                     @endphp
-                                    <li class="list-group-item pull-left" style="{{$float}}">
+                                    <li class="chat_container list-group-item pull-left" style="{{$float}}">
                                          {{--<img src="{{url('/')}}{{ str_replace('public/', '',isset( $chat->from_user->image) ? $chat->from_user->image : '')  }}" id="image_file" class="img-responsive img-circle" 
                                          alt=" {{ isset($chat->from_user->name)? $chat->from_user->name : '' }} " width="50px" height="50px">--}} &nbsp;
                                            <div id="image_file" class="chat_photo img-responsive img-circle" style="background-image: url('{{url('/')}}{{ str_replace('public/', '',isset( $chat->from_user->image) ? $chat->from_user->image : '')  }}');"></div>
