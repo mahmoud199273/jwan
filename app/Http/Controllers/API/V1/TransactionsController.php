caf9941b0f2da0cff07bef27cb5609fbef280336
ahmed badawy <ahmedbadawy@cems-it.com>
2019-03-05 14:45:55 +0200

    payment issues fixed

--- app/Http/Controllers/API/V1/TransactionsController.php
+++ app/Http/Controllers/API/V1/TransactionsController.php
@@ -28,7 +28,7 @@ class TransactionsController extends Controller
 
     function __construct(Request $request, TransactionsTransformer $transactionstransformer){
         App::setlocale($request->lang);
-    	$this->middleware('jwt.auth', ["except"=>["notifyDB"]]);
+    	$this->middleware('jwt.auth', ["except"=>["getCheckoutId","notifyDB"]]);
         $this->transactionstransformer   = $transactionstransformer;
     }
 
@@ -331,5 +331,9 @@ class TransactionsController extends Controller
             Self::PaymentOptions["Link"].$request->input("resourcePath")
         );
     }
+
+
+    // private function getStatus
+
 
 }
