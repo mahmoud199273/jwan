2c362566fd9f5027c2b1f502b9e099413d0ba471
ahmed badawy <ahmedbadawy@cems-it.com>
2019-03-14 12:17:10 +0200

    finished some features & fixes

--- app/Transformers/ChatTransformer.php
+++ app/Transformers/ChatTransformer.php
@@ -15,6 +15,7 @@ class ChatTransformer extends Transformer
 			$type_array = array(1=>"string", 2=>"url", 3=>"image", 4=>"file", 5=>"action");
         return [
             'id'            => (int) $chat->id,
+            'type'       => (int) $chat->type,
 		        'from_user_id'  => (int) $chat->from_user_id,
 						'from'					=> User::where('id', $chat->from_user_id)->get()->first(),
 			      'to_user_id'  => (int) $chat->to_user_id,
@@ -22,7 +23,6 @@ class ChatTransformer extends Transformer
 						'offer_id'		=> (int) $chat->offer_id,
 						'campaign_id'		=> (int) $chat->campaign_id,
             'content'          => Crypt::decryptString($chat->content),
-            'type'       => (int) $chat->type,
 						'type_title'	=> $type_array[(int)$chat->type],
 						'created_date'      => Carbon::parse($chat->created_at)->toDateTimeString(),
 						'created_date_string'      => Carbon::createFromTimeStamp(strtotime($chat->created_at))->diffForHumans(),
